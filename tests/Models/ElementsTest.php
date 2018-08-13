e7bc85ec76efc197ffec178f383233c3c7933f2a
Artem Garnyshev <is.perfect.possible@gmail.com>
2018-08-13 20:02:32 +0700

    Elements array bulk updates

--- tests/Models/ElementsTest.php
+++ tests/Models/ElementsTest.php
@@ -148,9 +148,9 @@ class ElementsTest extends TestCase
     }
 
     /**
-     * @group current
+     * @group bulk
      */
-    public function test_bulk_simple_operations()
+    public function test_bulk_simple_update()
     {
         $schema = $this->createSchema($this->user->backend, [
             [
@@ -178,6 +178,101 @@ class ElementsTest extends TestCase
 
         foreach ($newElements as $element) {
             $this->assertEquals($element->fields['stringSingleField'], 'new-title');
+        }
+
+        $schema->delete();
+    }
+
+    /**
+     * @group bulk
+     */
+    public function test_bulk_array_append_updates()
+    {
+        $additionalValues = ['4', '5', '6', '7'];
+        $schema = $this->createSchema($this->user->backend, [
+            [
+                'name' => 'stringMultipleField',
+                'type' => SchemaFieldTypes::STRING,
+                'multiple' => true
+            ]
+        ]);
+
+        for ($i = 0; $i < 5; $i++) {
+            $field = [];
+
+            for ($j = $i; $j < 5; $j++) {
+                $field[] = (string) $j;
+            }
+
+            $element = Element::create($schema->id, ['stringMultipleField' => $field], $this->user->backend);
+            $elements[$element->id] = $element;
+        }
+
+        Element::update(array_keys($elements), ['stringMultipleField' => [
+            'action' => 'append',
+            'value' => $additionalValues
+        ]], $schema, $this->user->backend);
+
+        $newElements = Element::get($schema, $this->user->backend, [
+            'where' => [
+                'id' => [
+                    '$in' => array_keys($elements)
+                ]
+            ]
+        ]);
+
+        foreach ($newElements as $updatedElement) {
+            $oldElement = $elements[$updatedElement->id];
+            $correntFieldValue = array_values(array_unique(array_merge($oldElement->fields['stringMultipleField'], $additionalValues)));
+            $this->assertEquals($updatedElement->fields['stringMultipleField'], $correntFieldValue);
+        }
+
+        $schema->delete();
+    }
+
+    /**
+     * @group bulk
+     */
+    public function test_bulk_array_remove_updates()
+    {
+        $excessValues = ['0', '1'];
+        $schema = $this->createSchema($this->user->backend, [
+            [
+                'name' => 'stringMultipleField',
+                'type' => SchemaFieldTypes::STRING,
+                'multiple' => true
+            ]
+        ]);
+
+        for ($i = 0; $i < 5; $i++) {
+            $field = [];
+
+            for ($j = $i; $j < 5; $j++) {
+                $field[] = (string) $j;
+            }
+
+            $element = Element::create($schema->id, ['stringMultipleField' => $field], $this->user->backend);
+            $elements[$element->id] = $element;
+        }
+
+        Element::update(array_keys($elements), ['stringMultipleField' => [
+            'action' => 'remove',
+            'value' => $excessValues
+        ]], $schema, $this->user->backend);
+
+        $newElements = Element::get($schema, $this->user->backend, [
+            'where' => [
+                'id' => [
+                    '$in' => array_keys($elements)
+                ]
+            ]
+        ]);
+
+        foreach ($newElements as $updatedElement) {
+            $oldElement = $elements[$updatedElement->id];
+            $correntFieldValue = array_values(array_diff($oldElement->fields['stringMultipleField'], $excessValues));
+            sort($updatedElement->fields['stringMultipleField']);
+            $this->assertEquals($updatedElement->fields['stringMultipleField'], $correntFieldValue);
         }
 
         $schema->delete();
