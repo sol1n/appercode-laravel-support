b0f0eaa542141da9739cfbc82a1940a814bd3f59
Artem Garnyshev <is.perfect.possible@gmail.com>
2019-11-20 18:47:14 +0700

    User creation

--- src/User.php
+++ src/User.php
@@ -25,7 +25,11 @@ class User
                     'type' => 'POST',
                     'url' => $backend->server . $backend->project . '/login/byToken'
                 ];
-
+            case 'create':
+                return [
+                    'type' => 'POST',
+                    'url' => $backend->server . $backend->project . '/users'
+                ];
             default:
                 throw new \Exception('Can`t find method ' . $name);
         }
@@ -33,12 +37,26 @@ class User
 
     public function __construct(Backend $backend, array $data)
     {
-        $this->id = $data['userId'] ?? null;
+        $this->id = $data['userId'] ?? $data['id'] ?? null;
         $this->token = $data['sessionId'] ?? null;
         $this->refreshToken = $data['refreshToken'] ?? null;
         $this->role = $data['roleId'] ?? null;
         $this->backend = $backend;
 
         return $this;
+    }
+
+    public static function create(Backend $backend, array $fields)
+    {
+        $method = self::methods($backend, 'create');
+
+        $json = self::jsonRequest([
+            'method' => $method['type'],
+            'json' => (object) $fields,
+            'headers' => ['X-Appercode-Session-Token' => $backend->token()],
+            'url' => $method['url']
+        ]);
+
+        return new User($backend, $json);
     }
 }
